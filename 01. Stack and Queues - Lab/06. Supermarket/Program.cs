51ae8ca5433bdd4d965835bdf85ae06d9b833439
Valentin Karakashev <116033694+ValchoPalcho@users.noreply.github.com>
2024-01-16 13:34:47 +0200

    Changed!

---
+++ 01. Stack and Queues - Lab/06. Supermarket/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _06._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            Queue<string> customers = new Queue<string>();
+
+            while (command != "End")
+            {
+                if (command == "Paid")
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, customers));
+                    customers.Clear();
+                }
+                else
+                {
+                    customers.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{customers.Count} people remaining.");
+        }
+    }
+}
